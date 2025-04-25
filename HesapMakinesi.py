dac141ea0b55b806b2b4d5ca652d2ff614ae9883
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2025-04-25 17:00:26 +0300

    added numbers

--- HesapMakinesi.py
+++ HesapMakinesi.py
@@ -33,5 +33,35 @@ multiply_button.grid(row=5,column=4)
 division_button = tkinter.Button(text="÷",command=lambda: click_button("/"))
 division_button.grid(row=4,column=4)
 
+#numbers
+button_1 = tkinter.Button(text="1",command=lambda: click_button("1"))
+button_1.grid(row=7,column=1)
+
+button_2 = tkinter.Button(text="2",command=lambda: click_button("2"))
+button_2.grid(row=7,column=2)
+
+button_3 = tkinter.Button(text="3",command=lambda: click_button("3"))
+button_3.grid(row=7,column=3)
+
+button_4 = tkinter.Button(text="4",command=lambda: click_button("4"))
+button_4.grid(row=6,column=1)
+
+button_5 = tkinter.Button(text="5",command=lambda: click_button("5"))
+button_5.grid(row=6,column=2)
+
+button_6 = tkinter.Button(text="6",command=lambda: click_button("6"))
+button_6.grid(row=6,column=3)
+
+button_7 = tkinter.Button(text="7",command=lambda: click_button("7"))
+button_7.grid(row=5,column=1)
+
+button_8 = tkinter.Button(text="8",command=lambda: click_button("8"))
+button_8.grid(row=5,column=2)
+
+button_9 = tkinter.Button(text="9",command=lambda: click_button("9"))
+button_9.grid(row=5,column=3)
+
+button_0 = tkinter.Button(text="0",command=lambda: click_button("0"))
+button_0.grid(row=8,column=2)
 
 window.mainloop()
