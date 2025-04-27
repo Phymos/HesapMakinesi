590b0bdaca7f148fefbcfc4367e467c41137b203
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2025-04-27 22:03:02 +0300

    added square root

--- HesapMakinesi.py
+++ HesapMakinesi.py
@@ -49,6 +49,13 @@ def square():
     space.delete("1.0","end")
     space.insert("1.0",space_text)
 
+def square_root():
+    global space,space_text,result
+    result = result ** 0.5
+    space_text = str(result)
+    space.delete("1.0","end")
+    space.insert("1.0",space_text)
+
 space = tkinter.Text(height=2,width=20)
 space.grid(row=1,column=1,columnspan=4)
 
@@ -116,5 +123,8 @@ inverse_button.grid(row=4,column=1)
 
 square_button = tkinter.Button(text="x²",command=square)
 square_button.grid(row=4,column=2)
+
+square_root_button = tkinter.Button(text="²√",command=square_root)
+square_root_button.grid(row=4,column=3)
 
 window.mainloop()
